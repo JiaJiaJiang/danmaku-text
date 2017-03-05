e0d90b50e5480e42cbe03ea80378ec27febb885d
luojia <luojia@luojia.me>
2017-03-06 00:27:27 +0800

    no message

--- src/danmaku-text.js
+++ src/danmaku-text.js
@@ -31,25 +31,24 @@ function init(DanmakuFrame,DanmakuFrameModule){
 			super(frame);
 			this.list=[];//danmaku object array
 			this.indexMark=0;//to record the index of last danmaku in the list
-			this.resetTunnel();
+			this.tunnel=new tunnelManager();
 			this.paused=true;
 			this.defaultStyle={//these styles can be overwrote by the 'font' property of danmaku object
 				fontStyle: null,
-				fontWeight: 600,
+				fontWeight: 300,
 				fontVariant: null,
-				color: "#fbfbfb",
+				color: "#fff",
 				lineHeight: null,//when this style is was not a number,the number will be the same as fontSize
 				fontSize: 30,
 				fontFamily: "Arial",
 				strokeWidth: 1,//outline width
 				strokeColor: "#000",
-				shadowBlur: 10,
+				shadowBlur: 5,
 				shadowColor: "#000",
 				shadowOffsetX:0,
 				shadowOffsetY:0,
 				fill:true,//if the text should be filled
 				reverse:false,
-				speed:5,
 				opacity:1,
 			};
 
@@ -69,6 +68,7 @@ function init(DanmakuFrame,DanmakuFrameModule){
 				allowLines:false,//allow multi-line danmaku
 				screenLimit:0,//the most number of danmaku on the screen
 				clearWhenTimeReset:true,//clear danmaku on screen when the time is reset
+				speed:5,
 			}
 		}
 		start(){
@@ -78,14 +78,17 @@ function init(DanmakuFrame,DanmakuFrameModule){
 		pause(){
 			this.paused=true;
 		}
-		stop(){
-			this.COL.clear();//clear the canvas
-		}
 		load(d){
 			if(!d || d._!=='text'){return false;}
-			const ind=dichotomy(this.list,d.time,0,this.list.length-1,false);
-			this.list.splice(ind,0,d);
-			if(ind<=this.indexMark)this.indexMark++;
+			let t=d.time,ind,arr=this.list;
+			if(arr.length===0 || (t<arr[0].time))ind=0;
+			else if(t>=arr[arr.length-1].time)ind=arr.length;
+			else{ind=dichotomy(arr,d.time,0,arr.length-1,false)}
+			arr.splice(ind,0,d);
+			if(ind<this.indexMark)this.indexMark++;
+			//round d.size to prevent Iifinity loop in tunnel
+			d.size=(d.size+0.5)|0;
+			if(d.size===NaN || d.size===Infinity)d.size=this.defaultStyle.fontSize;
 			return true;
 		}
 		loadList(danmakuArray){
@@ -101,23 +104,16 @@ function init(DanmakuFrame,DanmakuFrameModule){
 			if(i<this.indexMark)this.indexMark--;
 			return true;
 		}
-		resetTunnel(){
-			this.tunnels={
-				right:[],
-				left:[],
-				bottom:[],
-				top:[],
-			};
-		}
-		draw(){
+		draw(force){
 			if(!this.enabled)return;
 			//find danmaku from indexMark to current time
+			if(!force&&((this.danmakuMoveTime==cTime)||this.paused))return;
 			const cTime=this.frame.time,
 					cHeight=this.COL.canvas.height,
 					cWidth=this.COL.canvas.width,
 					ctx=this.COL.context;
 			let t,d;
-			if(this.list.length)
+			if(!force&&this.list.length)
 			for(;(d=this.list[this.indexMark])&&(d.time<=cTime);this.indexMark++){//add new danmaku
 				if(this.options.screenLimit>0 && this.layer.childNodes.length>=this.options.screenLimit)break;//break if the number of danmaku on screen has up to limit
 				if(document.hidden)continue;
@@ -136,50 +132,40 @@ function init(DanmakuFrame,DanmakuFrameModule){
 
 				t.prepare();
 				//find tunnel number
-				const size=t.style.height,tnum=this.getTunnel(d.mode,size);
-				t.tunnelNumber=tnum;
+				const size=t.style.height,tnum=this.tunnel.getTunnel(t,this.COL.canvas.height);
 				//calc margin
 				let margin=(tnum<0?0:tnum)%cHeight;
-				t.style.setPositionPoint(t.style.width/2,0);
 				switch(d.mode){
 					case 0:case 1:case 3:{
-						t.style.top=margin;break;
+						t.style.y=margin;break;
 					}
 					case 2:{
-						t.style.top=cHeight-margin;
+						t.style.y=cHeight-margin-t.style.height-1;
 					}
 				}
-				
-				tunnel[tnum]=((t.style.top+size)>cHeight)?
-								cHeight-t.style.top-1:
-								size;
+				if(d.mode>1)t.style.x=(cWidth-t.style.width)/2;
 				this.layer.appendChild(t);
 			}
 
 			//const cRange=this._clearRange;
 			//calc all danmaku's position
+			this.danmakuMoveTime=cTime;
+			this._clearCanvas();
 			for(t of this.layer.childNodes){
-				this.danmakuMoveTime=cTime;
-				if(t.drawn){
-					ctx.clearRect(t.style.x-t.estimatePadding,t.style.y-t.estimatePadding,t._cache.width,t._cache.height);
-				}else{t.drawn=true;}
-
 				switch(t.danmaku.mode){
 					case 0:case 1:{
-						const direc=t.danmaku.mode;
-						t.style.x=(direc?(cWidth+t.style.width/2):(-t.style.width/2))
-									+(direc?-1:1)*this.frame.rate*520*(cTime-t.time)/t.font.speed/1000;
-						if((direc||t.style.x<-t.style.width) || (direc&&t.style.x>cWidth+t.style.width)){//go out the canvas
+						const Mright=!t.danmaku.mode;
+						t.style.x=(Mright?(cWidth+t.style.width):(-t.style.width))
+									+(Mright?-1:1)*this.frame.rate*520*(cTime-t.time)/this.options.speed/1000;
+						if((Mright&&t.style.x<-t.style.width) || (!Mright&&t.style.x>cWidth+t.style.width)){//go out the canvas
 							this.removeText(t);
-						}else if(t.tunnelNumber>=0 && ((direc||(t.style.x+t.style.width/2)+30<cWidth) || (direc&&(t.style.x-t.style.width/2)>30))){
-							delete this.tunnels[tunnels[t.danmaku.mode]][t.tunnelNumber];
-							t.tunnelNumber=-1;
+						}else if(t.tunnelNumber>=0 && ((Mright&&(t.style.x+t.style.width)+30<cWidth) || (!Mright&&t.style.x>30))){
+							this.tunnel.removeMark(t);
 						}
 						break;
 					}
 					case 2:case 3:{
-						t.style.x=cWidth/2;
-						if((cTime-t.time)>t.font.speed*1000/this.frame.rate){
+						if((cTime-t.time)>this.options.speed*1000/this.frame.rate){
 							this.removeText(t);
 						}
 					}
@@ -198,54 +184,47 @@ function init(DanmakuFrame,DanmakuFrameModule){
 				}
 			}
 		}
-		getTunnel(tid,size){//get the tunnel index that can contain the danmaku of the sizes
-			let tunnel=this.tunnels[tunnels[tid]],tnum=-1,ti=0,
-				cHeight=this.COL.canvas.height;
-			if(size>cHeight)return -1;
-
-			while(tnum<0){
-				for(let i2=0;i2<size;i2++){
-					if(tunnel[ti+i2]!==undefined){//used
-						ti+=i2+tunnel[ti+i2];
-						break;
-					}else if(((ti+i2)%cHeight)===0){//new page
-						ti+=i2;
-						break;
-					}else if(i2===size-1){//get
-						tnum=ti;
-						break;
-					}
-				}
-
+		_evaluateIfFullClearMode(){
+			return false;
+		}
+		_clearCanvas(forceFull){
+			if(forceFull||this._evaluateIfFullClearMode()){
+				this.COL.clear();
+				return;
 			}
-			return tnum;
+			let ctx=this.COL.context
+			for(let t of this.layer.childNodes){
+				if(t.drawn){
+					ctx.clearRect(t.style.x-t.estimatePadding,t.style.y-t.estimatePadding,t._cache.width,t._cache.height);
+				}else{t.drawn=true;}
+			}
 		}
 		removeText(t){//remove the danmaku from screen
 			this.layer.removeChild(t);
+			this.tunnel.removeMark(t);
 			t.danmaku=null;
-			(t.tunnelNumber>=0)&&(delete this.tunnels[tunnels[t.danmaku.mode]][t.tunnelNumber]);
 			t.removeTime=Date.now();
 			this.COL_GraphCache.push(t);
 		}
 		resize(){
 			this.COL.adjustCanvas();
-			this.draw();
+			this.draw(true);
 		}
 		clear(){//clear danmaku on the screen
-			for(t of this.layer.childNodes){
+			for(let t of this.layer.childNodes){
 				if(t.danmaku)this.removeText(t);
 			}
-			this.resetTunnel();
+			this.tunnel.reset();
+			this._clearCanvas(true);
 		}
 		time(t){//reset time,you should invoke it when the media has seeked to another time
 			this.indexMark=dichotomy(this.list,t,0,this.list.length-1,true);
 			if(this.options.clearWhenTimeReset){this.clear();}
 			else{this.resetTimeOfDanmakuOnScreen();}
 		}
-		resetTimeOfDanmakuOnScreen(){
+		resetTimeOfDanmakuOnScreen(cTime=this.frame.time){
 			//cause the position of the danmaku is based on time
 			//and if you don't want these danmaku on the screen to disappear,their time should be reset
-			const cTime=Date.now();
 			for(let t of this.layer.childNodes){
 				if(!t.danmaku)continue;
 				t.time=cTime-(this.danmakuMoveTime-t.time);
@@ -263,27 +242,88 @@ function init(DanmakuFrame,DanmakuFrameModule){
 		}
 		enable(){//enable the plugin
 			this.layer.style.hidden=false;
-			this.canvas.hidden=false;
 		}
 		disable(){//disable the plugin
 			this.layer.style.hidden=true;
-			this.canvas.hidden=false;
 			this.clear();
+		}
+	}
+
+	class tunnelManager{
+		constructor(){
+			this.reset();
+		}
+		reset(){
+			this.right={};
+			this.left={};
+			this.bottom={};
+			this.top={};
+		}
+		getTunnel(tobj,cHeight){//get the tunnel index that can contain the danmaku of the sizes
+			let tunnel=this.tunnel(tobj.danmaku.mode),
+				size=tobj.style.height,
+				ti=0,
+				tnum=-1;
+			if(typeof size !=='number' || size<0){
+				console.error('Incorrect size:'+size);
+				size=1;
+			}
+			if(size>cHeight)return 0;
+
+			while(tnum<0){
+				for(let t=ti+size-1;ti<=t;){
+					if(tunnel[ti]){//used
+						ti+=tunnel[ti].tunnelHeight;
+						break;
+					}else if((ti!==0)&&(ti%(cHeight-1))===0){//new page
+						ti++;
+						break;
+					}else if(ti===t){//get
+						tnum=ti-size+1;
+						break;
+					}else{
+						ti++;
+					}
+				}
+			}
+			tobj.tunnelNumber=tnum;
+			tobj.tunnelHeight=((tobj.style.y+size)>cHeight)?cHeight-tobj.style.y-1:size;
+			this.addMark(tobj);
+			return tnum;
+		}
+		addMark(tobj){
+			let t=this.tunnel(tobj.danmaku.mode);
+			if(!t[tobj.tunnelNumber])t[tobj.tunnelNumber]=tobj;
+		}
+		removeMark(tobj){
+			let t,tun=tobj.tunnelNumber;
+			if(tun>=0&&(t=this.tunnel(tobj.danmaku.mode))[tun]===tobj){
+				delete t[tun];
+				tobj.tunnelNumber=-1;
+			}
+		}
+		tunnel(id){
+			return this[tunnels[id]];
 		}
 	}
 
 	const tunnels=['right','left','bottom','top'];
 
 	function dichotomy(arr,t,start,end,position=false){
-		if(arr.length===0)return -1;
-		let m,base=position?-1:1;
-		while(start < end){
+		if(arr.length===0)return 0;
+		let m,e=end;
+		while(start < end){//dichotomy
 			m=(start+end)>>1;
-			if(t<arr[m].time)start=m+1;
-			else end=m;
+			if(t>=arr[m].time)start=m+1;
+			else{end=m;}
 		}
-		while(arr[start+base] && (arr[start+base].time===arr[start].time))
-			start+=+base;
+		if(position){
+			while(start<e && (arr[start+=1].time===arr[start].time)){}
+		}else{
+			while(start>0 && (arr[start-1].time===arr[start].time)){
+				start-=1;
+			}
+		}
 		return start;
 	}
 
