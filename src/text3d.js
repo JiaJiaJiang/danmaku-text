74b8baf6c78bd33c9e0c14cc019052563ccbc4e5
luojia <luojia@luojia.me>
2017-05-11 15:47:16 +0800

    no message

--- src/text3d.js
+++ src/text3d.js
@@ -10,8 +10,7 @@ class Text3d extends Template{
 	constructor(dText){
 		super(dText);
 		this.supported=false;
-		let c3d=this.c3d=dText.canvas3d;
-		c3d=document.createElement('canvas');//the canvas
+		let c3d=this.c3d=dText.canvas3d=document.createElement('canvas');
 		c3d.classList.add(`${dText.randomText}_fullfill`);
 		c3d.id=`${dText.randomText}_text3d`;
 		dText.context3d=c3d.getContext('webgl')||c3d.getContext('experimental-webgl');//the canvas3d context
